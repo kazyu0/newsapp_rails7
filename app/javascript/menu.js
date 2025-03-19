fdd9bf9dc60af04ef579e29418fdb59b87bb25d2
kazyu0 <kazumakimijima.sb@gmail.com>
2025-03-19 10:28:59 +0900

    メニューいろ色替え

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,3 +1,10 @@
 window.addEventListener('turbo:load', function(){
+  const menu = document.getElementById("menu1");
 
+  menu.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color: orange;");
+  });
+  menu.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color: orange;");
+  });
 });
